07b1e33ad821bbbe7a875a073067efcc981ecc99
Isabelle <meguminperson@gmail.com>
2023-03-22 00:17:20 +0000

    commit messages are hard lol

--- life/board.py
+++ life/board.py
@@ -1,2 +1,37 @@
+from . import Cell
+from typing import Final, List
+
 class Board:
-    pass
+    def __init__(self, width: int, height: int) -> None:
+        """
+            Initialises a Board object.
+        """
+
+        self.width: Final[int] = width
+        self.height: Final[int] = height
+        self.board: List[List[Cell]] = []
+
+
+    def _generate_empty_board(self) -> List[List[Cell]]:
+        """
+            Generate an empty board with the provided width & height.
+
+            Returns
+            -------
+            A List[List[Cell]].
+        """
+        
+        empty_board: List[List[Cell]] = []
+        
+        for x in range(self.width):
+            empty_board[x] = []
+            
+            for y in range(self.height):
+                new_cell = Cell(x, y)
+                empty_board[x].append(new_cell)
+        
+        return empty_board
+
+
+    def get_cell(self, x: int, y: int) -> Cell:
+        return self.board[x][y]

8d4af31ebe59cb3b2ecd565b851b364b8b161a8e
Isabelle <meguminperson@gmail.com>
2023-03-22 01:29:03 +0000

    testing the blinker demo
    (it's broken)

--- the-game-of-life.py
+++ the-game-of-life.py
@@ -1,8 +1,17 @@
 import life
 
 game_board = life.Board(25, 10)
+
+game_board._board[5][5] = life.Cell(5, 5, game_board, True)
+game_board._board[6][5] = life.Cell(5, 6, game_board, True)
+game_board._board[7][5] = life.Cell(5, 7, game_board, True)
+
 print(game_board)
 
-cell_at_0_0 = game_board._get_cell(0, 0)
-print(cell_at_0_0)
-print(cell_at_0_0.alive)
+game_board.permutate()
+
+print(game_board)
+
+game_board.permutate()
+
+print(game_board)

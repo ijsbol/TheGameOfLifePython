8197865246784b92db874b3f0a6d3bf4883f2d68
Isabelle <meguminperson@gmail.com>
2023-03-22 04:13:27 +0000

    merge conflicts are a pain

--- life/board.py
+++ life/board.py
@@ -30,8 +30,8 @@ class Board:
 
         self.__debug: Final[bool] = debug
         self.__display_as_numbers: Final[bool] = debug if debug else display_as_numbers
-        self.____advanced_number_display: Final[bool] = advanced_number_display
-        self.____label_axis: Final[bool] = label_axis
+        self.__advanced_number_display: Final[bool] = advanced_number_display
+        self.__label_axis: Final[bool] = label_axis
 
     def __str__(self) -> str:
         """
@@ -42,7 +42,7 @@ class Board:
 
         # Set the top of the str board
         board_width_including_edges = self.width + 2
-        if self.____label_axis:
+        if self.__label_axis:
             formatted_board += "  " + "".join([str(i) for i in range(self.width)]) + "\n"
             formatted_board += BOARD_TOP_CHAR * board_width_including_edges
         else:
@@ -56,7 +56,6 @@ class Board:
                 if x == 0:
                     # Start of a new row
                     if self.__label_axis:
-                    if self.__label_axis:
                         formatted_board += str(y)
                     formatted_board += BOARD_LEFT_SIDE_CHAR
 
@@ -64,11 +63,12 @@ class Board:
                     cell_neighbours = self._get_cell_neighbours(cell)
                     alive_neighbours = self._count_alive_cells_in_neighbour_row(cell_neighbours)
                     formatted_board += str(alive_neighbours)
-                elif self.__advanced_number_display:
+                
                 elif self.__advanced_number_display:
                     cell_neighbours = self._get_cell_neighbours(cell)
                     alive_neighbours = self._count_alive_cells_in_neighbour_row(cell_neighbours)
                     formatted_board += f"[{'a' if cell.alive else 'd'}{str(alive_neighbours)}]"
+                
                 else:
                     formatted_board += str(cell)
 
